5b8fc743456b3090a0bea7ce549f07f5180bd31c
Bernhard Werner <bernhard.werner@hm.edu>
2023-12-19 09:03:59 +0100

    Added templat efor responsive cindy widget. Canvas will be as large as viewport. (In particular, it will be the size of the iframe it is loaded into.)

--- egdod.cjs
+++ egdod.cjs
@@ -1,15 +1,19 @@
-canvasPoly = apply(screenbounds(), #.xy); //LO, RO, RU, LU
-canvasCorners = {
-    "tl": canvasPoly_1,
-    "tr": canvasPoly_2,
-    "br": canvasPoly_3,
-    "bl": canvasPoly_4
-};
-canvasCenter  = 0.5 * canvasCorners.tl + 0.5 * canvasCorners.br;
-canvasWidth   = dist(canvasCorners.tl, canvasCorners.tr);
-canvasHeight  = dist(canvasCorners.tl, canvasCorners.bl);
-[canvasLeft, canvasTop] = canvasCorners.tl;
-[canvasRight, canvasBottom] = canvasCorners.br;
+updateLayout() := (
+    canvasPoly = apply(screenbounds(), #.xy); //LO, RO, RU, LU
+    canvasCorners = {
+        "tl": canvasPoly_1,
+        "tr": canvasPoly_2,
+        "br": canvasPoly_3,
+        "bl": canvasPoly_4
+    };
+    canvasCenter  = 0.5 * canvasCorners.tl + 0.5 * canvasCorners.br;
+    canvasWidth   = dist(canvasCorners.tl, canvasCorners.tr);
+    canvasHeight  = dist(canvasCorners.tl, canvasCorners.bl);
+    [canvasLeft, canvasTop] = canvasCorners.tl;
+    [canvasRight, canvasBottom] = canvasCorners.br;
+);
+updateLayout();
+
 screenMouse() := [(mouse().x - canvasLeft) / canvasWidth, (mouse().y - canvasBottom) / canvasHeight];
 
 
