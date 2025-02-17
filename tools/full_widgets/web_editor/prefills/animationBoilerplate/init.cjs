b4509c3e86b5714a812d081a217309385097ac9d
Bernhard Werner <bernhard.werner@hm.edu>
2025-02-17 10:55:57 +0100

    Extended animation boilerplate prefill for web editor

--- tools/full_widgets/web_editor/prefills/animationBoilerplate/init.cjs
+++ tools/full_widgets/web_editor/prefills/animationBoilerplate/init.cjs
@@ -2,7 +2,16 @@ startDelay = 0.3;
 pause = if(renderMode == RENDERMODES.FRAMES, 1, 0.1);
 
 trackData = [
-    1, pause
+    1, pause,   // 01   
+    1, pause,   // 02   
+    1, pause,   // 03   
+    1, pause,   // 04   
+    1, pause,   // 05   
+    1, pause,   // 06   
+    1, pause,   // 07   
+    1, pause,   // 08   
+    1, pause,   // 09   
+    1, pause    // 10   
 ];
 
 delayedSetup() := (
