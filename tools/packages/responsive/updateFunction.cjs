9bc1488b030d3161ad91c009890a9a5a5ec3b705
Bernhard Werner <bernhard.werner@hm.edu>
2025-02-17 11:08:21 +0100

    Updated responsive package to use anchors as animation

--- tools/packages/responsive/updateFunction.cjs
+++ tools/packages/responsive/updateFunction.cjs
@@ -1,18 +1,28 @@
 updateLayout() := (
     canvasPoly = apply(screenbounds(), #.xy); //LO, RO, RU, LU
-    canvasCorners = {
-        "tl": canvasPoly_1,
-        "tr": canvasPoly_2,
-        "br": canvasPoly_3,
-        "bl": canvasPoly_4
-    };
-    canvasCenter  = 0.5 * canvasCorners.tl + 0.5 * canvasCorners.br;
-    canvasWidth   = dist(canvasCorners.tl, canvasCorners.tr);
-    canvasHeight  = dist(canvasCorners.tl, canvasCorners.bl);
-    [canvasLeft, canvasTop] = canvasCorners.tl;
-    [canvasRight, canvasBottom] = canvasCorners.br;
-  
+
+    canvasAnchors = [
+        canvasPoly_4,
+        0.5 * canvasPoly_4 + 0.5 * canvasPoly_3,
+        canvasPoly_3,
+    
+        0.5 * canvasPoly_4 + 0.5 * canvasPoly_1,
+        0.5 * canvasPoly_4 + 0.5 * canvasPoly_2,
+        0.5 * canvasPoly_3 + 0.5 * canvasPoly_2,
+    
+        canvasPoly_1,
+        0.5 * canvasPoly_1 + 0.5 * canvasPoly_2,
+        canvasPoly_2
+    ];
+    
+    
+    canvasCenter  = canvasAnchors_5;
+    canvasWidth   = dist(canvasAnchors_1, canvasAnchors_3);
+    canvasHeight  = dist(canvasAnchors_1, canvasAnchors_7);
+    [canvasLeft, canvasTop] = canvasAnchors_7;
+    [canvasRight, canvasBottom] = canvasAnchors_3;
+    
     customLayout();
-  );
-  
-  customLayout() := ();
+);
+
+customLayout() := ();
